5daa7789fc227a46ad3e77363401d1f9657f6432
Raymond Degbe <198380402+mrdegbe@users.noreply.github.com>
2025-06-25 19:31:20 +0100

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -3,9 +3,17 @@
 from fastapi import FastAPI
 from .api import router
 from .database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="ASHCOM Job Tracking API", version="1.0.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or ["https://ashcom-dashboard.vercel.app"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
